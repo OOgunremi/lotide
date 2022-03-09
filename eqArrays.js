03632cfc969a2aec7ce83d7220413421fdbe7b15
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-03-08 18:48:04 -0700

    adding eqArrays which compares Arrays to the folder

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😄👏😄 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡💔😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
