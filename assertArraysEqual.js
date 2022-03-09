ddf5b66c85c35fb4c276d866c80532e1510bd5fb
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-03-08 19:11:40 -0700

    adding a function that asserts if two arrays are equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😄👏😄 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡💔😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`😄👏😄 Assertion Passed, [${arrayOne}] is equal to [${arrayTwo}].`);
+  } else {
+    console.log(`😡💔😡 Assertion Failed [${arrayOne}] is not equal to [${arrayTwo}].`);
+  }
+};
+assertArraysEqual([1, 2, 3], [1, 3, 3])
+assertArraysEqual([1, 2, 3], [1, 2, 3])
