17e9484b85f2702fafb6ab05e916c010a791bd13
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-03-14 11:01:28 -0600

    Added eqArraysTest.js and refactored eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😄👏😄 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡💔😡 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 const eqArrays = function(arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) {
     return false;
@@ -18,8 +10,4 @@ const eqArrays = function(arrayOne, arrayTwo) {
   }
   return true;
 };
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
